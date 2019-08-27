84118b67c7913f7d182c2699dd12b69c71b5a9c5
Stoyan Shukerov <stoichik@gmail.com>
2019-08-27 13:26:58 -0700

    Imported main.scss on each page. Set up nav and footer.

--- src/pages/contact/contact.js
+++ src/pages/contact/contact.js
@@ -1,1 +1,2 @@
+import '../../assets/css/main.scss';
 import '../../assets/css/pages/_contact.scss'
--- src/pages/services/services.js
+++ src/pages/services/services.js
@@ -1,2 +1,3 @@
+import '../../assets/css/main.scss';
 import '../../assets/css/pages/_services.scss'
 
