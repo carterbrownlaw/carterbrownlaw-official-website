c5c088cdb651134ff5e6ffa4dcf05895f04dbe5e
Stoyan Shukerov <stoichik@gmail.com>
2019-08-27 17:13:55 -0700

    Adding delicious-hamburgers package. Not done yet.
    Going to need jquery to do this properly and fast.

--- src/pages/about/about.js
+++ src/pages/about/about.js
@@ -5,6 +5,23 @@ import '../../assets/css/pages/_about.scss'
 // import { StubHelper } from '../../javascript/stubHelper.js';
 import { StubHelper } from '../../javascript/stubHelper.js';
 
+// menu js probably needs to be global
+const navBtn = document.getElementById('nav-btn');
+const navItems = document.getElementById('nav-items');
+navBtn.addEventListener('click', () => {
+  if (navBtn.classList.contains('active')) {
+    navBtn.classList.remove('active');
+    navItems.classList.remove('items-active');
+    navItems.classList.add('items-hidden');
+  }
+  else {
+    navBtn.classList.add('active');
+    navItems.classList.remove('items-hidden');
+    navItems.classList.add('items-active');
+  }
+});
+
+console.log(navItems);
 let mainContainer = document.getElementsByTagName('main')[0];
 let stubber = new StubHelper(mainContainer);
 stubber.stub(5);
