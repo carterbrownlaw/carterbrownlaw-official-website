522d49ca5a409d2ab03957875e04ea06e4f76dec
Stoyan Shukerov <stoichik@gmail.com>
2019-08-29 14:05:38 -0700

    Adding content to testimonial page.
    Testimonials are loaded on the fly. Should be easy to add them and edit
    them.

--- src/pages/testimonials/testimonials.js
+++ src/pages/testimonials/testimonials.js
@@ -4,5 +4,35 @@ import '../../assets/css/pages/_testimonials.scss';
 // JS imports
 import { commonJS } from '../../javascript/commonJS.js';
 
+const mainContainer = new commonJS(false);
 
-const mainContainer = new commonJS;
+// load all testimonials
+const testimonialsContext = require.context('../../assets/content/testimonials/', true, /\.json$/);
+const testimonialFiles = testimonialsContext.keys().map((filename) => testimonialsContext(filename));
+
+const testiContainer = document.getElementById('testimonial-container');
+testimonialFiles.forEach((t) => {
+  testiContainer.appendChild(createTestimonial(t));
+});
+
+function createTestimonial(t) {
+  // create elements
+  const tElement = document.createElement('div');
+  const tText = document.createElement('p');
+  const tAuthor = document.createElement('p');
+  
+  // add styles
+  tElement.classList.add('testimonial');
+  tAuthor.classList.add('testimonial-author');
+
+  // add content
+  tText.innerHTML = t.text;
+  tAuthor.innerHTML = '~ ' + t.author;
+  
+  // append things
+  tElement.appendChild(tText);
+  tElement.appendChild(tAuthor);
+
+  return tElement;
+}
+
