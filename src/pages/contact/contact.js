d2da9f119fea00032869f379963723400a5a3400
Stoyan Shukerov <stoichik@gmail.com>
2019-08-29 11:28:26 -0700

    Adding a google map. Important: restrict API key.
    When going live make sure the key is restricted.

--- src/pages/contact/contact.js
+++ src/pages/contact/contact.js
@@ -4,5 +4,29 @@ import '../../assets/css/pages/_contact.scss';
 // JS imports
 import { commonJS } from '../../javascript/commonJS.js';
 
-const mainContainer = new commonJS;
+const mainContainer = new commonJS(false);
+setupMap();
 
+function setupMap() {
+  const loadGoogleMapsApi = require('load-google-maps-api');
+  const mapContainer = document.getElementById('map');
+  // The location of carter brown law offices
+  var uluru = {
+    lat: -25.344,
+    lng: 131.036
+  };
+  // The map, centered at carter brown law
+  // var map = new googleMaps.Map( mapContainer, {
+  //   zoom: 4,
+  //   center: uluru
+  // });
+  loadGoogleMapsApi({key: 'AIzaSyCkS58fwUabI3HKffisb7dyOGuR3XRmqFo'}).then((googleMaps) => {
+    // console.log(mapContainer);
+    new googleMaps.Map(mapContainer, {
+      zoom: 4,
+      center: uluru
+    });
+  }).catch(function (error) {
+    console.error(error)
+  });
+}
