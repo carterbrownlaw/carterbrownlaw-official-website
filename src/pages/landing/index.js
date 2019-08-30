e585f709620d9ec13540b3c18b392b723af52083
Stoyan Shukerov <stoichik@gmail.com>
2019-08-30 13:21:49 -0700

    Adding tiny slider for index page.
    Package is very small and seems to work nicely. Might row my own vanilla
    JS solution later.

--- src/pages/landing/index.js
+++ src/pages/landing/index.js
@@ -1,16 +1,37 @@
+import '../../../node_modules/tiny-slider/src/tiny-slider.scss';
 import '../../assets/css/main.scss';
 import '../../assets/css/pages/_index.scss';
 
 // JS imports
 import { importTestimonials } from '../../javascript/helpers.js';
 import { commonJS } from '../../javascript/commonJS.js';
+import { tns } from '../../../node_modules/tiny-slider/src/tiny-slider.js';
 
 const mainContainer = new commonJS();
 
 const testimonialFiles = importTestimonials();
 const testiContainer = document.getElementById('testimonials-container');
-testimonialFiles.forEach((t) => {
-  testiContainer.appendChild(createTestimonial(t));
+const numTestimonials = testimonialFiles.length;
+
+// testimonialFiles.forEach((t) => {
+//   testiContainer.appendChild(createTestimonial(t));
+// });
+for (let i = 0; i < numTestimonials; i++) {
+  testiContainer.appendChild(createTestimonial(testimonialFiles[i]));
+}
+
+
+const slider = tns({
+  // container: '#testimonials-container',
+  container: testiContainer,
+  items: 1,
+  slideBy: 'page',
+  autoplay: true,
+  speed: 400,
+  controls: false,
+  navPosition: 'bottom',
+  mouseDrag: true,
+  autoplayButtonOutput: false
 });
 
 function createTestimonial(t) {
